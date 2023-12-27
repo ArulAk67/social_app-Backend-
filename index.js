e014a00f00bc9ba5ad80e3a20e959a6f56b24f8e
Arulkumar A <126221799+ArulAk67@users.noreply.github.com>
2023-12-27 23:39:34 +0530

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
     next();
 });
 app.use(express.json())
-app.use(cors({origin:"https://658c626b77b8f50ea3334291--adorable-blini-bbe727.netlify.app"}))
+app.use(cors({origin:"https://mediagram-social.netlify.app"}))
 // app.use(cors())
 app.use(cookieParser())
 
